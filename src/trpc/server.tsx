7e0788ef8cae2550de2ad33d3b1a387cf2f608fc
ayushkumar3820 <kumarayush382000@gamil.com>
2025-07-29 22:21:30 +0530

    ds

--- src/trpc/server.tsx
+++ src/trpc/server.tsx
@@ -1,9 +1,16 @@
-import 'server-only'; 
+// Remove the direct import
+// import 'server-only';
+
 import { createTRPCOptionsProxy } from '@trpc/tanstack-react-query';
 import { cache } from 'react';
 import { createTRPCContext } from './init';
 import { makeQueryClient } from './query-client';
 import { appRouter } from './routers/_app';
+
+// Only import server-only in server environment
+if (typeof window === 'undefined') {
+  require('server-only');
+}
 
 export const getQueryClient = cache(makeQueryClient);
 export const trpc = createTRPCOptionsProxy({
@@ -11,6 +18,5 @@ export const trpc = createTRPCOptionsProxy({
   router: appRouter,
   queryClient: getQueryClient,
 });
-
 
 export const caller = appRouter.createCaller(createTRPCContext);
